b0d8d2638eb9ecab7456846cffc7e315991d037d
Andrey Azov <andrey.azov@gmail.com>
2015-10-24 06:04:41 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    entry: "./js/main.js",
+    resolve: {
+        alias: {
+            "marionette": "backbone.marionette"
+        }
+    },
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    node: {
+        fs: "empty"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" },
+            { test: /\.html/, loader: "underscore-template-loader" },
+            { test: /\.hbs/, loader: "handlebars-template-loader" }
+            //{ test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+            //{ test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+            //{ test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+        ]
+    }
+};
